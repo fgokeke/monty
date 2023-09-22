6b10d183ae4f7a9953a21cd242e73e8c20a50308
Munachi Iwundu <realjones2012@gmail.com>
2023-09-22 04:45:59 +0000

    Added other functions

---
+++ fm_mul.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * mul - multiplies two element together
+ * @stack: Pointer to the stack
+ * @line_number: Line number where the multiplication starts
+ * Return: nothing
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp_holder1, *temp_holder2;
+
+	(void) stack;
+	if (argument_file->stack_len < 2)
+	{
+		dprintf(2, "L%d: can't multiply, stack to short\n", line_number);
+		total_free_args();
+		exit(EXIT_FAILURE);
+	}
+
+	temp_holder1 = argument_file->head;
+	temp_holder2 = temp_holder1->next;
+
+	temp_holder2->n = temp_holder2->n * temp_holder1->n;
+	del_stack_node();
+
+	argument_file->stack_len--;
+}
+
+
